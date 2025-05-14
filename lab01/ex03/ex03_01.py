914bd0f0af95dcdd7056fbd9790e155ab6c411a9
unknown <thongtg7@gmail.com>
2025-05-14 15:51:37 +0700

    test commit

---
+++ lab01/ex03/ex03_01.py
@@ -0,0 +1,10 @@
+def tinhtongsochan(lst):
+    tong = 0
+    for num in lst:
+        if num %2 ==0:
+            tong +=num
+    return tong
+input_list = input("nhap vao cac so, cach nhau bang dau phay: ")
+numbers = list(map(int,input_list.split(',')))
+tongchan= tinhtongsochan(numbers)
+print("tong cac so chan: ",tongchan)
